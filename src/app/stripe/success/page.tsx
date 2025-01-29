f98b65bce7ba29741363dd4fcd6915bf31723b58
Jay Termulo <jetermulo@gmail.com>
2025-01-30 03:57:36 +0800

    bugfix: clear cart when successfully paid in stripe

--- src/app/stripe/success/page.tsx
+++ src/app/stripe/success/page.tsx
@@ -1,17 +1,27 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useShoppingCart } from "use-shopping-cart";
 
 const SuccessPage = () => {
+  const { clearCart } = useShoppingCart();
+
+  useEffect(() => {
+    clearCart();
+  }, [clearCart]);
+
   return (
-    <section className='py-72'>
+    <section className="py-72">
       <div className="container mx-auto">
-        <h3 className='text-center mb-4'>Your Payment was successful! Thank you!</h3>
-        <Link href='/'>
-          <button className='btn btn-primary mx-auto'>Back to homepage</button>
+        <h3 className="mb-4 text-center">
+          Your Payment was successful! Thank you!
+        </h3>
+        <Link href="/">
+          <button className="btn btn-primary mx-auto">Back to homepage</button>
         </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SuccessPage
+export default SuccessPage;
