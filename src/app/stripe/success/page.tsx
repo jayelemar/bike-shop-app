0a61cbde578eb61ce77888aa35ac4665341fca7c
Jay Termulo <jetermulo@gmail.com>
2025-01-30 04:11:13 +0800

    bugfix: infinite loop for clearCart

--- src/app/stripe/success/page.tsx
+++ src/app/stripe/success/page.tsx
@@ -1,19 +1,15 @@
 "use client";
-import { useRouter } from "next/navigation";
+
+import Link from "next/link";
 import React, { useEffect } from "react";
 import { useShoppingCart } from "use-shopping-cart";
 
 const SuccessPage = () => {
-  const router = useRouter();
   const { clearCart } = useShoppingCart();
 
   useEffect(() => {
     clearCart();
   }, []);
-
-  const handleClick = () => {
-    router.push("/");
-  };
 
   return (
     <section className="py-72">
@@ -21,10 +17,9 @@ const SuccessPage = () => {
         <h3 className="mb-4 text-center">
           Your Payment was successful! Thank you!
         </h3>
-
-        <button className="btn btn-primary mx-auto" onClick={handleClick}>
-          Back to homepage
-        </button>
+        <Link href="/">
+          <button className="btn btn-primary mx-auto">Back to homepage</button>
+        </Link>
       </div>
     </section>
   );
